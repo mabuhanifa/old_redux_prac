db782bd522948d460292cda9f09612c17261f01b
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-02 18:13:00 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { todoActions } from "./redux/actions/todoActions";
 
 function App() {
+  const dispatch = useDispatch();
+  const { todo } = useSelector((state) => state);
+  useEffect(() => {
+    dispatch(todoActions());
+  }, [dispatch]);
+  console.log(todo);
   return (
     <div>
       <div>home</div>
--- src/redux/actions/todoActions.js
+++ src/redux/actions/todoActions.js
@@ -5,10 +5,11 @@ import {
     TODO_SUCCESS
 } from "../constants/todoConstans";
 
-const todoActions = () => async (dispatch) => {
+export const todoActions = () => async (dispatch) => {
   dispatch({ type: TODO_REQUEST });
   try {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+
     dispatch({ type: TODO_SUCCESS, payload: res.data });
     console.log(res.data);
   } catch (error) {
