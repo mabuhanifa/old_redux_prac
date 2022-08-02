0ff572cd67c8cccc373ba4bbe04daf0815ff1ad6
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-02 17:59:27 +0600

    redux packages added

--- src/redux/actions/todoActions.js
+++ src/redux/actions/todoActions.js
@@ -1,10 +1,16 @@
-import { TODO_FAILED, TODO_REQUEST, TODO_SUCCESS } from "../constants/todoConstans";
+import axios from "axios";
+import {
+    TODO_FAILED,
+    TODO_REQUEST,
+    TODO_SUCCESS
+} from "../constants/todoConstans";
 
 const todoActions = () => async (dispatch) => {
   dispatch({ type: TODO_REQUEST });
   try {
-    const res = await axios
-    dispatch({ type: TODO_SUCCESS });
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    dispatch({ type: TODO_SUCCESS, payload: res.data });
+    console.log(res.data);
   } catch (error) {
     dispatch({ type: TODO_FAILED });
   }
